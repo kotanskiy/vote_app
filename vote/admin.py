499f5187c2e7d230f2e16db5c09d31a0f5c17c9d
Alexey Yefimenko <alesha95@ukr.net>
2017-07-06 20:45:07 +0300

    add somethings templates

--- vote/admin.py
+++ vote/admin.py
@@ -4,4 +4,3 @@ from django.contrib import admin
 from vote.models import Question, Choice
 
 admin.site.register(Question)
-admin.site.register(Choice)
--- vote/urls.py
+++ vote/urls.py
@@ -3,12 +3,8 @@ from . import views
 
 app_name = 'vote'
 urlpatterns = [
-    # ex: /vote/
-    url(r'^$', views.index, name='index'),
-    # ex: /vote/5/
-    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # ex: /vote/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # ex: /vote/5/vote/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
